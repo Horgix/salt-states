f4baa7388da3890e546c05f9238f63fc94f680a8
Alexis 'Horgix' Chotard <alexis.horgix.chotard@gmail.com>
2018-03-26 14:04:20 +0200

    users: remove alias for sudo docker

--- users/files/shell-config/aliases.sh
+++ users/files/shell-config/aliases.sh
@@ -92,7 +92,6 @@ alias ssh='TERM=rxvt ssh'
 alias irc='tmux attach -t irc'
 alias ip='ip -c'
 alias ssha='ssh-agent -t 12h | grep -v echo > ~/.ssh/ssh_${HOST}_${USER}.agent && source ~/.ssh/ssh_${HOST}_${USER}.agent && ssh-add'
-alias docker='sudo docker'
 
 # Dockerized tools <3
 
